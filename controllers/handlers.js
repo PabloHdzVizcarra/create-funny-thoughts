f3909360e5b20b298dac252224efd4a2fa78dbf2
Pablo Hernandez <pablourbanohdzvizcarra@gmail.com>
2020-10-11 18:56:59 -0500

    Fixed bug when deleting thoughts continuously

--- controllers/handlers.js
+++ controllers/handlers.js
@@ -1,21 +1,18 @@
 const { deleteElement, createItemInCollectionDB, getAllDataFromMongoDB } = require("../lib/operationsMongoDB");
 
 exports.sendForm = async (req, res) => {
-  const result = await createItemInCollectionDB(req.body)
-  console.log('CREATE DOC')
+  await createItemInCollectionDB(req.body)
 
   res.json(req.body);
 };
 
 exports.getDataFromDB = async (req, res) => {
   const dataFromMongoDB = await getAllDataFromMongoDB()
-  console.log('GET ALL DOCS IN COLLECTION')
   res.json(dataFromMongoDB);
 
 };
 
 exports.deleteElement = async (req, res) => {
   deleteElement(req.body)
-  console.log('DELETE DOC')
   res.json(req.body)
 };
